9430ae4d355dacc80142e15b6cd789452f6c6d00
amandakuan <amandakuan6@gmail.com>
2022-04-01 12:54:56 +0800

    4/1

--- src/main/java/com/kuan/score/Scoring.java
+++ src/main/java/com/kuan/score/Scoring.java
@@ -3,9 +3,21 @@ package com.kuan.score;
 import com.kuan.score.GraduateStudent;
 import com.kuan.score.Student;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scoring {
     public static void main(String[] args) {
-        Student tony = new Student("Tony",80,90);
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Tony",80,90));
+        students.add(new Student("Jack",10,40));
+        students.add(new GraduateStudent("Jane",90,75,90));
+
+        for(Student s : students){
+            s.print();
+        }
+
+        /*Student tony = new Student("Tony",80,90);
         Student jack = new Student("Jack",10,40);
         GraduateStudent jane =
                 new GraduateStudent("Jane",90,75,90);
@@ -14,6 +26,6 @@ public class Scoring {
         //tony.math = 90;
         tony.print();
         jack.print();
-        jane.print();
+        jane.print();*/
     }
 }
