e0ae7b439e554772f012f46d077f6ee918402408
amandakuan <amandakuan6@gmail.com>
2022-05-27 13:03:52 +0800

    5/27 Guess Number

--- src/main/java/com/kuan/ui/GuessFrame.java
+++ src/main/java/com/kuan/ui/GuessFrame.java
@@ -4,27 +4,43 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
     //Fields
+    JButton button = new JButton("GUESS");
+    JLabel label = new JLabel("???");
+    JTextField number = new JTextField(10);
+    Random random = new Random();
+    int secret = random.nextInt(10)+1;
     //Constructors
     public GuessFrame(){
         super();//一定要在第一行
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button = new JButton("Hi");
-        JLabel label = new JLabel("www");
+        System.out.println("Secret: "+secret);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //System.out.println("Hello");
-                label.setText("Ohla!!!");
+                //label.setText("Ohla!!!");
+                int num = Integer.parseInt(number.getText());
+                System.out.println(num);
+                if(num > secret){
+                    label.setText("Smaller");
+                }else if(num<secret){
+                    label.setText("Bigger");
+                }else {
+                    label.setText("Bingo!! the anser is "+num);
+                    System.out.println("End");
+                }
             }
         });
         setLayout(new FlowLayout());
-        add(label);
+        add(number);
         add(button);
+        add(label);
         setVisible(true);
 
     }
